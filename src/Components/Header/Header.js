b6a8c7e11c980555145c42beaf4aaf380a1d2b6a
roopehub <roope.heinonen94@gmail.com>
2019-11-22 01:22:05 +0200

    changed header image

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -15,7 +15,7 @@ class Header extends Component {
             <div className={classes.Header}>
                 <h2 onClick={() => this.flyTo(map, [60.205, 24.933907])}>Programming</h2>
                 <h2 onClick={() => this.flyTo(map, [60.199, 24.951029])}>Projects</h2>
-                <img src="https://scontent-hel2-1.xx.fbcdn.net/v/t1.0-9/68476700_10213995935364103_19694624075415552_n.jpg?_nc_cat=105&_nc_oc=AQm_pNi-tFNKQmKRt3Az4a33w2t65Q-1aDsXjWrNvb-AQXFCQpd8Hm7sTVobDq3rwqY&_nc_ht=scontent-hel2-1.xx&oh=a05159e2287fa1ee50960e89ba441e97&oe=5DC90B9D" />
+                <img src="https://scontent-hel2-1.xx.fbcdn.net/v/t1.0-9/p960x960/69092796_10213995935404104_6269808544489406464_o.jpg?_nc_cat=107&_nc_ohc=sKEvN568t-kAQnEaIpNJnJZi40BGQyy7aNxK6Yqv4IAxaJs6v7UXFMdDw&_nc_ht=scontent-hel2-1.xx&oh=39fd732f630fbb93e83bc0408ddd84d3&oe=5E435C4D" />
                 <h2 onClick={() => this.flyTo(map, [60.207, 24.962420])}>Geography</h2>
                 <h2 onClick={() => this.flyTo(map, [60.23, 25.012640])}>Me</h2>
             </div>
