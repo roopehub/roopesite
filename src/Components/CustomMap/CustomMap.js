04b95f10c042b25f203a464548efb95e3517d9a4
roopehub <roope.heinonen94@gmail.com>
2020-07-28 21:09:35 +0300

    huutista

--- src/Components/CustomMap/CustomMap.js
+++ src/Components/CustomMap/CustomMap.js
@@ -7,8 +7,8 @@ import CustomMarkers from '../CustomMarkers/CustomMarkers';
 
 class CustomMap extends Component {
     state = {
-        startPos: [60.26, 25.012640],
-        startZoom: 13
+        startPos: [61.0269511, 25.65371217],
+        startZoom: 20
     }
     render() {
         return (
